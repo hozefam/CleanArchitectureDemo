143c1a145249d2e8a1f897761d5c7fae165021b5
Hozefa Mangalorewala <hozefam@gmail.com>
2025-04-06 22:41:27 +0530

    Add blog management features and remove weather forecast
    Updated project to support Entity Framework and Swagger.
    Introduced `ApiControllerBase` and `BlogController` for blog queries.
    Removed `WeatherForecastController` and related files.
    Added database connection string and initial migration for `Blogs` table.
    Updated `BlogVM` with a parameterless constructor for usability.

---
+++ CleanArchitectureDemo.API/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitectureDemo.Application.Blogs.Queries.GetBlogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureDemo.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BlogController : ApiControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> GetAllAsync()
+		{
+			var blogs = await Mediator.Send(new GetBlogQuery());
+			return Ok(blogs);
+		}
+	}
+}
