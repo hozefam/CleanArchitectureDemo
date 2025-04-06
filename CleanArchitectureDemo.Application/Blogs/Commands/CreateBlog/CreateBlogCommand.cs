0332b31962d613e8026490229b1b2977d919ac18
Hozefa Mangalorewala <hozefam@gmail.com>
2025-04-06 20:21:38 +0530

    Add CreateBlog command and refactor GetBlogById query
    - Introduced `CreateBlogCommand` record with parameters for Title, Content, and Author.
    - Implemented `CreateBlogCommandHandler` to handle command execution and mapping to `BlogVM`.
    - Refactored `GetBlogByIdQuery` to use a record instead of a class.
    - Removed the `Blogs\Commands\CreateBlog\` folder from the project structure.

---
+++ CleanArchitectureDemo.Application/Blogs/Commands/CreateBlog/CreateBlogCommand.cs
@@ -0,0 +1,7 @@
+﻿using CleanArchitectureDemo.Application.Blogs.Queries.GetBlogs;
+using MediatR;
+
+namespace CleanArchitectureDemo.Application.Blogs.Commands.CreateBlog
+{
+	public record CreateBlogCommand(string Title, string Content, string Author) : IRequest<BlogVM>;
+}
