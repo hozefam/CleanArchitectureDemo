b5cf296a0a1750dba42b200b9e122709cd234f4a
Hozefa Mangalorewala <hozefam@gmail.com>
2025-04-06 20:31:59 +0530

    Add UpdateBlog command and handler implementation
    Created `UpdateBlogCommand` and `UpdateBlogCommandHandler` to handle blog updates using MediatR. Updated `IBlogRepository` to modify the `UpdateAsync` method signature for better identification of blog entries. Removed the `UpdateBlog` folder from the project structure.

---
+++ CleanArchitectureDemo.Application/Blogs/Commands/UpdateBlog/UpdateBlogCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace CleanArchitectureDemo.Application.Blogs.Commands.UpdateBlog
+{
+	public record UpdateBlogCommand(int Id, string Title, string Content, string Author) : IRequest<int>;
+}
--- CleanArchitectureDemo.Domain/Repository/IBlogRepository.cs
+++ CleanArchitectureDemo.Domain/Repository/IBlogRepository.cs
@@ -7,7 +7,7 @@ namespace CleanArchitectureDemo.Domain.Repository
 		Task<List<Blog>> GetAllAsync();
 		Task<Blog?> GetByIdAsync(int id);
 		Task<Blog> CreateAsync(Blog blog);
-		Task<Blog> UpdateAsync(Blog blog);
+		Task<int> UpdateAsync(int id, Blog blog);
 		Task<bool> DeleteAsync(int id);
 	}
 }
